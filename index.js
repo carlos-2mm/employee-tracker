e9b4a6e86369b5deebbfe7a083ae114eedc1595b
Carlos Macias <cimm21187@gmail.com>
2023-10-01 22:14:14 -0400

    Added switch cases to handle viewing of departments, roles, and employees

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ async function collectInput() {
     ],
   });
 
+  switch (action) {
+        case 'View all departments':
+            const departments = await queries.getAllDepartments();
+            console.table(departments);
+            break;
+        case 'View all roles':
+            const roles = await queries.getAllRoles();
+            console.table(roles);
+            break;
+        case 'View all employees':
+            const employees = await queries.getAllEmployees();
+            console.table(employees);
+            break;
+  }
+
     collectInput();
 }
 
