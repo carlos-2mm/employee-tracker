de4adb2e785eac36e77235aa9630dec8000abfb4
Carlos Macias <cimm21187@gmail.com>
2023-10-03 15:33:50 -0400

    Refactor MySQL connection to use callback and log success

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
-module.exports = connection;
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database.');
+  });
+
+
+module.exports = db;
