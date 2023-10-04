63fd35f10a299a9c3dc74365f5ddd12c5f6f5d9a
Carlos Macias <cimm21187@gmail.com>
2023-10-03 20:21:58 -0400

    Refactor index file for enhanced clarity and error handling. Adding console.table de pendency

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
-const inquirer = require("inquirer");
-const queries = require("./db/queries");
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const {
+    viewAllDepartments,
+    viewAllRoles,
+    viewAllEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole
+} = require('./db/queries');
 
 async function collectInput() {
-  const { action } = await inquirer.prompt({
-    type: "list",
-    name: "action",
-    message: "What would you like to do?",
-    choices: [
-      "View all departments",
-      "View all roles",
-      "View all employees",
-      "Add a department",
-      "Add a role",
-      "Add an employee",
-      "Update an employee role",
-      "Exit",
-    ],
-  });
+    try {
+        const response = await inquirer.prompt([
+            {
+                name: 'action',
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit'
+                ]
+            }
+        ]);
 
-  switch (action) {
-        case 'View all departments':
-            const departments = await queries.getAllDepartments();
-            console.table(departments);
-            break;
-        case 'View all roles':
-            const roles = await queries.getAllRoles();
-            console.table(roles);
-            break;
-        case 'View all employees':
-            const employees = await queries.getAllEmployees();
-            console.table(employees);
-            break;
-  }
-
-    collectInput();
+        switch (response.action) {
+            case 'View all departments':
+                await viewAllDepartments(collectInput);
+                break;
+            case 'View all roles':
+                await viewAllRoles(collectInput);
+                break;
+            case 'View all employees':
+                await viewAllEmployees(collectInput);
+                break;
+            case 'Add a department':
+                await addDepartment(collectInput);
+                break;
+            case 'Add a role':
+                await addRole(collectInput);
+                break;
+            case 'Add an employee':
+                await addEmployee(collectInput);
+                break;
+            case 'Update an employee role':
+                await updateEmployeeRole(collectInput);
+                break;
+            case 'Exit':
+                process.exit();
+                break;
+        }
+        collectInput();
+    } catch (error) {
+        console.error('An error occurred:', error);
+        collectInput();
+    }
 }
 
 collectInput();
